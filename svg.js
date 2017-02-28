10bc0b3b420219bcc67385221c1e0c7d3f1253e1
Anya Keller <anyakeller@betelgeuse.local>
2017-02-28 13:54:38 -0500

    added line functionality

--- svg.js
+++ svg.js
@@ -1,17 +1,34 @@
 var pic=document.getElementById("vimage");
 
 var c = document.createElementNS(
-"http://www.w3.org/2000/svg",
-"circle");
+    "http://www.w3.org/2000/svg",
+    "circle");
 c.setAttribute("cx",0);
 c.setAttribute("cy",0);
 c.setAttribute("r","100");
 c.setAttribute("fill","red");
 c.setAttribute("stroke","black");
 
+var x = -1;
+var y = -1;
+
 var drawCirc = function(evt){
-    var x = evt.offsetX;//clientX - rect.left,
-    var y = evt.offsetY;//clientY - rect.top
+    if (x < 0){
+        x = evt.offsetX;//clientX - rect.left,
+        y = evt.offsetY;//clientY - rect.top
+    }else{
+        var line = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "line");
+        line.setAttribute("x1",x);
+        line.setAttribute("y1",y);
+        x = evt.offsetX;//clientX - rect.left,
+        y = evt.offsetY;//clientY - rect.top
+        line.setAttribute("x2",x);
+        line.setAttribute("y2",y);
+        line.setAttribute("stroke","black");
+        pic.appendChild(line);
+    };
     var circ = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "circle");
@@ -25,7 +42,7 @@ var drawCirc = function(evt){
 
 var clear = function(){
     while (pic.lastChild) {
-            pic.removeChild(pic.lastChild);
+        pic.removeChild(pic.lastChild);
     };
 };
 
