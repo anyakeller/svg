efd11fc9c0ca7e67af2f571ac3b8a80230ec6605
Anya Keller <anyakeller@betelgeuse.local>
2017-02-28 14:01:27 -0500

    cleaned code

--- svg.js
+++ svg.js
@@ -1,14 +1,5 @@
 var pic=document.getElementById("vimage");
 
-var c = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "circle");
-c.setAttribute("cx",0);
-c.setAttribute("cy",0);
-c.setAttribute("r","100");
-c.setAttribute("fill","red");
-c.setAttribute("stroke","black");
-
 var x = -1;
 var y = -1;
 
@@ -44,6 +35,8 @@ var clear = function(){
     while (pic.lastChild) {
         pic.removeChild(pic.lastChild);
     };
+    x = -1;
+    y = -1;
 };
 
 
